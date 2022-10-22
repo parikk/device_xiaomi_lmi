c4df5931addfa63c52c9a877e36076760baa0717
parikk <fariqhfebrian18@gmail.com>
2022-10-22 15:15:14 +0700

    lmi: Build Google Camera Go

--- device.mk
+++ device.mk
@@ -159,6 +159,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    GCamGOPrebuilt \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
